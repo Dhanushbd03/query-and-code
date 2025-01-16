266350bb6302a37be18f056594ce532bfe11ff8e
Dhanush B D <119742887+Dhanushbd03@users.noreply.github.com>
2025-01-16 12:23:28 +0000

    basic setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Routes, Route } from 'react-router-dom'
+import { routes } from './routes/Routes'
+
+function App() {
+  return (
+    <Routes>
+      {routes.map((route) => (
+        <Route 
+          key={route.path}
+          path={route.path}
+          element={route.element}
+        />
+      ))}
+    </Routes>
+  )
+}
+
+export default App
---
+++ frontend/src/components/auth/AuthButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
+const AuthButton = () => {
+  return (
+    <button>
+      <FaSignInAlt />
+      <span>Signin</span>
+    </button>
+  );
+};
+
+export default AuthButton;
